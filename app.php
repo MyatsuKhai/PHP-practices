bc496b6a0b22248735224d56e678b88f54399d89
myatsukhai <myatsukai@gmail.com>
2024-03-23 10:06:11 +0630

    made a little changes.

--- app.php
+++ app.php
@@ -1,16 +1,15 @@
 <?php
 //?name=Bob&age=22
 //print_r($_POST);
-print_r($_GET);
+//print_r($_GET);
 //print_r($_REQUEST);
 
 
-//$users = ['Alice','Bob','Chris'];
-//print_r($users);
+$users = ['Alice','Bob','Chris'];
+print_r($users);
  
-//echo"<br>";
+echo"<br>";
 
-//let user ={name:'Allice',age:22}
-
-//$users=['name' => 'Alice','age'=>22];
-//print_r($users);
+//let user ={name:'Allice',age:22};
+$users=['name' => 'Alice','age'=>22];
+print_r($users);
--- c.php
+++ c.php
@@ -1,4 +1,5 @@
 <?php
 session_start();
 unset($_SESSION['user']);
+
 //setcookie("theme","",time()-1);
